876a0cb18666d0917b923fcbcf698f024f8a2510
Xulio-Xulio <61099971+Xulio-Xulio@users.noreply.github.com>
2020-11-19 12:09:13 -0300

    fix

---
+++ script.js
@@ -0,0 +1,22 @@
+const elements = document.querySelectorAll(`[data-anima]`)
+const animacaoClass = `animacao`
+
+function animaScroll() {
+    /* Tamanho da janela a ser usado */
+    const pageTop = window.pageYOffset+((window.innerHeight*4)/6)
+    /* Percorre os elementos [data-anima] */
+    elements.forEach(function(element){
+       /* Adiciona ou remove animação baseado no pageTop */
+        if(pageTop > element.offsetTop){
+            element.classList.add(animacaoClass)
+        }else{
+            element.classList.remove(animacaoClass)
+        }
+    })
+}
+
+if(elements.length) {
+    window.addEventListener(`scroll`, function(){
+        animaScroll()
+    })
+} 
